482b92b477a9e2f1457c6bae1da2a59b05800989
Yaser GHaderi <yaserghaderi.web@gmail.com>
2024-06-17 20:15:07 +0330

    start

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
-# this file added with compatible modules and codes
+# # this file added with compatible modules and codes
+#
+# import ssl
+# import streamlit as st
+# from streamlit_gsheets import GSheetsConnection
+#
+# url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+#
+# conn = st.connection("gsheets", type=GSheetsConnection)
+#
+# data = conn.read(spreadsheet=url, usecols=[0, 1])
+# st.dataframe(data)
 
-import ssl
+# example/st_app_gsheets_using_service_account.py
+
 import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
-url = "https://docs.google.com/spreadsheets/d/1JDy9md2VZPz4JbYtRPJLs81_3jUK47nx6GYQjgU8qNY/edit?usp=sharing"
+st.title("new title")
 
 conn = st.connection("gsheets", type=GSheetsConnection)
+df = conn.read(worksheet="Sheet2")
 
-data = conn.read(spreadsheet=url, usecols=[0, 1])
-st.dataframe(data)
+st.dataframe(df)
